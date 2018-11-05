632df7c8abe4e312995d185f1ac1b77fed6b432e
Cody Jacques <cody@frase.io>
2018-11-05 10:25:55 -0500

    add scenario routes and tests.

--- rest/open_dota.py
+++ rest/open_dota.py
@@ -1,5 +1,8 @@
 import requests
 from rest.api import API
+from log import get_logger
+
+LOG = get_logger('rest', 'rest.log')
 
 class OpenDota(API):
     '''
@@ -9,7 +12,9 @@ class OpenDota(API):
 
     rest_api = "https://api.opendota.com/api"
 
-    def _get(self, command: str, options = {}):
+    def _get(self, command: str, options = None):
+        if options is None:
+            options = {}
         base_url = "{}{}".format(self.rest_api, command)
 
         if self.key_id is not None:
@@ -40,3 +45,34 @@ class OpenDota(API):
         Returns top currently ongoing live games
         '''
         return self._get('/live')
+
+    # scenarios apis
+    def get_item_timings(self, parameters = None):
+        '''
+        Paramenters:
+            item: item name (replace space with underscore, e.g. "spirit_vessel")
+            hero_id: id of hero
+
+        If no parameters are passed, get_scenarios returns all combinations of hero and item.
+        '''
+        return self._get('/scenarios/itemTimings', parameters)
+
+    def get_lane_roles(self, parameters = None):
+        '''
+        Parameters:
+            lane_role: Filter by lane role 1-4 (Safe, Mid, Off, Jungle)
+            hero_id: id of hero
+
+        If no parameters are passed get all combinations
+        If both parameters are passed, get list of results that are win rates by time slice. e.g. [{'hero_id': 62, 'lane_role': 1, 'time': 900, 'games': '394', 'wins': '201'}, {'hero_id': 62, 'lane_role': 1, 'time': 1800, 'games': '4586', 'wins': '1926'}, ...]
+        '''
+        return self._get('/scenarios/laneRoles', parameters)
+
+    def get_misc_scenarios(self, parameters = None):
+        '''
+        Parameters:
+            scenario: pos_chat_1min,neg_chat_1min,courier_kill,first_blood
+
+        if a scenario is passed, returns a list of win rate for each region
+        '''
+        return self._get('/scenarios/misc', parameters)
--- test/rest/test_open_dota.py
+++ test/rest/test_open_dota.py
@@ -14,6 +14,54 @@ class TestOpenDota(unittest.TestCase):
         if len(live) > 0:
             assert len(live[0]['players']) > 0
 
+    def test_get_item_timings(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_item_timings()
+
+        assert len(scenarios) > 0
+
+    def test_get_item_timings_item(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_item_timings({'item': 'ancient_janggo'})
+
+        assert len(scenarios) > 0
+
+    def test_get_item_timings_hero(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_item_timings({'hero_id': 120})
+
+        assert len(scenarios) > 0
+
+    def test_get_item_timings_both(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_item_timings({'hero_id': 120, 'item': 'ancient_janggo'})
+
+        assert len(scenarios) == 1
+
+    def test_get_lane_roles(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_lane_roles()
+
+        assert len(scenarios) > 0
+
+    def test_get_lane_roles_both(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_lane_roles({'hero_id': 62, 'lane_role': 1})
+
+        assert len(scenarios) > 0
+
+
+    def test_get_misc_scenarios(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_misc_scenarios()
+
+        assert len(scenarios) > 0
+
+    def test_get_misc_scenarios_param(self):
+        dota = Rest('config.yaml').OpenDota
+        scenarios = dota.get_misc_scenarios({'scenario': 'courier_kill'})
+
+        assert len(scenarios) > 0
 
 if __name__ == '__main__':
     unittest.main()
