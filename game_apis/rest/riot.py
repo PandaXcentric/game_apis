e3b0f642241f1b8ac7058f47192f87b10a6a9434
smstojanovic <stojanovich.stevan@live.com>
2019-01-02 13:36:08 +1100

    adding limiter in riot code (1 second)

--- game_apis/rest/riot.py
+++ game_apis/rest/riot.py
@@ -7,6 +7,7 @@ LOG = get_logger('rest', 'rest.log')
 # https://developer.riotgames.com/api-methods/
 class Riot(API):
     ID = 'RIOT'
+    LIMIT = 1
 
     def __init__(self, config, region=None, sandbox=False, local_config=False):
         super().__init__(config, sandbox, local_config)
@@ -15,6 +16,7 @@ class Riot(API):
             region = 'na1'
 
         self.rest_api = "https://{}.api.riotgames.com".format(region)
+
 
     def _get(self, command: str, options = None):
         if options is None:
@@ -34,7 +36,11 @@ class Riot(API):
 
             base_url = "{}&{}={}".format(base_url, key, val)
 
+        self.check_limiter()
+
         resp = requests.get(base_url)
+
+        self.reset_limiter()
 
         if resp.status_code != 200:
             LOG.error("%s: Status code %d", self.ID, resp.status_code)
