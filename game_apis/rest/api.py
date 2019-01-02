6b7f872a5b4d6398b45a812f3fb300e549a7bbc0
smstojanovic <stojanovich.stevan@live.com>
2019-01-02 13:35:51 +1100

    adding rate limiter code into api class.

--- game_apis/rest/api.py
+++ game_apis/rest/api.py
@@ -1,13 +1,18 @@
 import os
 import yaml
+from datetime import datetime
+from time import sleep
 
 class API:
     ID = 'NONE'
+    LIMIT = 0
 
     def __init__(self, config, sandbox=False, local_config=False):
         path = os.path.dirname(os.path.abspath(__file__))
         self.key_id, self.key_secret, self.key_passphrase = None, None, None
         self.sandbox = sandbox
+        self.ref_time = datetime(1900,1,1)
+
         if not config:
             config = "config.yaml"
 
@@ -26,3 +31,15 @@ class API:
                     self.key_passphrase = data[self.ID.lower()]['key_passphrase']
         except (KeyError, FileNotFoundError, TypeError):
             pass
+
+    def check_limiter():
+        # before we get the URL, let's check we are complying with the rate limiter.
+        cur_time = datetime.now()
+        time_delta = (cur_time - self.ref_time).total_seconds()
+
+        if time_delta < self.LIMIT:
+            sleep(time_delta - self.LIMIT)
+
+    def reset_limiter():
+        self.ref_time = datetime.now()
+        
