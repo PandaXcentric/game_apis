54cff71b439cff4330e3158175a92cd4c6dea4ef
smstojanovic <stojanovich.stevan@live.com>
2019-01-04 14:43:21 +1100

    - adding fortnite unit tests - fixing rest

--- game_apis/rest/rest.py
+++ game_apis/rest/rest.py
@@ -4,7 +4,7 @@ from game_apis.rest.open_dota import OpenDota
 from game_apis.rest.riot import Riot
 from game_apis.rest.riot_esports import RiotEsports
 from game_apis.rest.pubg import Pubg
-from game_apis.rest.fortnite_tracker import Fortnite_Tracker
+from game_apis.rest.fortnite_tracker import FortniteTracker
 
 class Rest:
     def __init__(self, config=None, region=None, sandbox=False, local_config=False, ignore_limiter=False):
