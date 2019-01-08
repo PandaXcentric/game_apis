c7311d5db07e071e8ec27d9e39e11f8a93df1c3b
Cody Jacques <cody@frase.io>
2019-01-07 21:31:44 -0500

    finish rainbow 6 routes.

--- game_apis/rest/rainbow_six_siege.py
+++ game_apis/rest/rainbow_six_siege.py
@@ -10,6 +10,13 @@ class Rainbow6Siege(API):
     r6s_url = 'https://api.r6stats.com/api/v1'
     platforms = ['ps4', 'xone', 'uplay']
 
+    # In this case unofficial denotes the fact that I'm hitting unofficial routes
+    # The data itself is legit
+    r6s_unofficial_url = 'https://r6stats.com/api'
+    unofficial_platforms = ['ps4', 'xone', 'pc']
+    unofficial_regions = ['all', 'ncsa', 'emea', 'apac']
+
+    # For some reason the get calls for the official url are slow, the unofficial is a lot faster
     def _get(self, player, platform='uplay', operators = False):
         req_url = '{}/{}/{}'.format(self.r6s_url, 'players', player)
 
@@ -29,9 +36,54 @@ class Rainbow6Siege(API):
         return resp.json()
 
 
+    def _get_unofficial(self, uri):
+        req_url = '{}/{}'.format(self.r6s_unofficial_url, uri)
+
+        resp = requests.get(req_url)
+
+        if resp.status_code != 200:
+            LOG.error("%s: Status code %d", self.ID, resp.status_code)
+            LOG.error("%s: Headers: %s", self.ID, resp.headers)
+            LOG.error("%s: Resp: %s", self.ID, resp.text)
+            resp.raise_for_status()
+
+        return resp.json()
+
+
     def get_player_info(self, player, platform=None):
         return self._get(player, platform=platform)
 
 
     def get_player_operators(self, player, platform=None):
         return self._get(player, platform=platform, operators=True)
+
+    def get_leaderboards(self, platform='pc', region='all', page=1):
+        return self._get_unofficial(
+            'leaderboards/seasonal/{}/{}?page={}'.format(platform, region, page)
+        )
+
+    def get_player_stats(self, player_id):
+        '''
+        The player id in this case is a guid you can get from the get_leaderboards call
+        You can also get this id from get_high_level_player_status ubisoft id
+        '''
+
+        return self._get_unofficial(
+            'stats/{}'.format(player_id)
+        )
+
+    def get_player_stats_seasonal(self, player_id):
+        '''
+        The player id in this case is a guid you can get from the get_leaderboards call
+        You can also get this id from get_high_level_player_status ubisoft id
+        '''
+
+        return self._get_unofficial(
+            'stats/{}/seasonal'.format(player_id)
+        )
+
+
+    def get_high_level_player_status(self, player_name, platform='pc'):
+        return self._get_unofficial(
+            'player-search/{}/{}'.format(player_name, platform)
+        )
--- test/rest/test_rainbow_six_siege.py
+++ test/rest/test_rainbow_six_siege.py
@@ -16,3 +16,31 @@ class TestRainbow6Siege(unittest.TestCase):
         info = r6s.get_player_operators('barzY-YT', 'uplay')
 
         assert len(info['operator_records']) > 0
+
+
+    def test_get_leaderboards(self):
+        r6s = Rest('config.yaml', ignore_limiter=True).Rainbow6Siege
+        leaders = r6s.get_leaderboards('pc', page=1)
+
+        assert len(leaders) == 100
+
+
+    def test_get_player_stats_seasonal(self):
+        r6s = Rest('config.yaml', ignore_limiter=True).Rainbow6Siege
+        player_stats = r6s.get_player_stats_seasonal('bedbbe83-015a-4e6f-babc-580c6e6ff9f3')
+
+        assert player_stats['username'] == 'SanalsBae'
+
+
+    def test_get_player_stats(self):
+        r6s = Rest('config.yaml', ignore_limiter=True).Rainbow6Siege
+        player_stats = r6s.get_player_stats('bedbbe83-015a-4e6f-babc-580c6e6ff9f3')
+
+        assert player_stats['username'] == 'SanalsBae'
+
+
+    def test_get_high_level_player_status(self):
+        r6s = Rest('config.yaml', ignore_limiter=True).Rainbow6Siege
+        player_stats = r6s.get_high_level_player_status('SanalsBae', 'pc')
+
+        assert player_stats[0]['ubisoft_id'] == 'bedbbe83-015a-4e6f-babc-580c6e6ff9f3'
